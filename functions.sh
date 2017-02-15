bc76472a67aaea242f53561f076b479a37324121
Giulio <giulio@MBP-di-Giulio.lan>
2017-02-15 19:10:25 +0100

    v0.8

--- functions.sh
+++ functions.sh
@@ -6,3 +6,6 @@
 # pg for PluGin
 # XX is a short code for your plugin, ex: ww for Weather Wunderground
 # You can use translations provided in the language folders functions.sh
+jv_pg_bitcoin () {
+	curl -s 'http://api.coindesk.com/v1/bpi/currentprice.json' | jq -r '.bpi.EUR.rate_float' | cut -b 1-5
+}
