7c611a013f76fe73dd80c5b430877b126203c23c
marioincesti <mario.incesti@vps30.com>
2017-02-17 19:53:48 +0100

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -7,6 +7,6 @@
 # XX is a short code for your plugin, ex: ww for Weather Wunderground
 # You can use translations provided in the language folders functions.sh
 jv_pg_webmusic () {
-	youtube-dl -o - "gvsearch1:$1 $2 $3 $4 $5" | mplayer -vo fbdev2 -zoom -xy 1920 -really-quiet -cache 30720 -cache-min 2 /dev/fd/3 3<&0 </dev/tty &
+	youtube-dl -o /tmp - "gvsearch1:$1 $2 $3 $4 $5" | mplayer -vo fbdev2 -zoom -xy 1920 -really-quiet -cache 30720 -cache-min 2 /dev/fd/3 3<&0 </dev/tty &
 
 }
